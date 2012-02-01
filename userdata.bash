5c1acc19ed950b07c838ae4e945c5ed664acc50d
Tass Skoudros <tass@skoudros.com>
2012-02-01 14:40:37 +0000

    update to userdata
    Signed-off-by: Tass Skoudros <tass@skoudros.com>

--- userdata.bash
+++ userdata.bash
@@ -1,6 +1,27 @@
 #!/bin/bash
-LOCAL_PATH="/opt/skystack"
-GITHUB_PATH="https://github.com/askoudros"
+# Skystack will fill in this information before sending this script to the cloud vendor
+export SS_SERVER_ID=<% SS_SERVER_ID %>
+export SS_STACK_ID=<% SS_STACK_ID %>
+
+export SS_ALIAS=<% SS_ALIAS %>
+export SS_HOST=<% SS_HOST %>
+
+# Each server has its own user account that it will use to interface with Skystack.
+export SS_APIUSER=<% SS_APIUSER %>
+export SS_APITOKEN=<% SS_APITOKEN %>
+export SS_APIPASS=<% SS_APIPASS %>
+
+# The following metadata can be used to add functionality based on this metadata. 
+
+export SS_BASE=<% SS_BASE %>
+export SS_ROLE=<% SS_ROLE %>
+export SS_ENVIRONMENT=<% SS_ENVIRONMENT %>
+export SS_JURISDICTION=<% SS_JURISDICTION %>
+export SS_CLOUD=<% SS_CLOUD %>
+
+export LOCAL_PATH="/opt/skystack"
+export GITHUB_PATH="https://github.com/askoudros"
+
 PREV_DIR=`pwd`
 mkdir -p $LOCAL_PATH; cd $LOCAL_PATH;
 mkdir -p archives sources cookbooks init etc backups bin logs run
@@ -27,4 +48,5 @@ cd $LOCAL_PATH/sources; unzip ../archives/chef-solo-init.zip; cd $LOCAL_PATH;
 ln -s $LOCAL_PATH/sources/`ls  ../sources | grep chef-solo-init` $LOCAL_PATH/chef-solo-init;
 chmod +x $LOCAL_PATH/chef-solo-init/setup; cd chef-solo-init;
 	
-./setup 
+`$LOCAL_PATH/chef-solo-init/setup` 
+ 
