473e4677f78ceb5f80ac9cbf79e0b026b0c6102c
Tass Skoudros <tass@skoudros.com>
2012-02-02 06:58:49 +0000

    minor update to fix ruby
    removed the ruby apt-get instruction using in its place ruby1.8. Definitely want to use RVM next.
    
    Signed-off-by: Tass Skoudros <tass@skoudros.com>

--- setup.bash
+++ setup.bash
@@ -5,6 +5,7 @@
 COOKBOOK_PATH="https://github.com/askoudros"
 LOCAL_PATH="/opt/skystack"
 
+# make sure our basepath is set, it should definitely be set in the bootstrap script but no harm in doing it again
 if [ ! -d $LOCAL_PATH ];then
 	mkdir_opt_skystack $LOCAL_PATH
 fi
@@ -16,11 +17,10 @@ get_cookbook "chef-mysql" $LOCAL_PATH $COOKBOOK_PATH
 get_cookbook "chef-skystack" $LOCAL_PATH $COOKBOOK_PATH
 get_cookbook "chef-collectd" $LOCAL_PATH $COOKBOOK_PATH
 
-
 chef_solo_config
+
 apt-get update
-apt-get -y install make unzip ruby ruby1.8 ruby1.8-dev libruby1.8-extras rubygems gcc lsb-release subversion
-gem update --system
+apt-get -y install make unzip ruby1.8 ruby1.8-dev libruby1.8-extras rubygems gcc lsb-release subversion
 
 gem sources -a http://gems.opscode.com
 gem install json chef ohai --no-ri --no-rdoc
@@ -28,7 +28,6 @@ gem install json chef ohai --no-ri --no-rdoc
 find /var/lib -name "chef-solo" -exec ln -s '{}' /usr/bin/chef-solo \; 
 
 chmod +x /usr/bin/chef-solo
-
 
 #curl -k -o /tmp/dna.json -u $SS_APIUSER:$SS_APITOKEN $HTTP://$SS_BASE/$SS_ALIAS/servers/$SS_SERVER_ID.json?action=dna_only
 
